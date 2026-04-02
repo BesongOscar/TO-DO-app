fa8839c42255f8a30e32c2ae8fbfa65b86a01808
Besong Oscar <wildeo963@gmail.com>
2026-04-02 20:31:48 +0100

    Remove broken persistence code - will use memory persistence

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, indexedDBLocalPersistence } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDbT9PXXLn3xpeuPeb3dRlVO6aKroSqslo",
@@ -13,5 +13,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
-auth.setPersistence(indexedDBLocalPersistence);
